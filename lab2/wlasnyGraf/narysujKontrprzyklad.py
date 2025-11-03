da67e7885c7fe2cae7063fc9e85d94b004f24b81
Jakub Kogut <jakub@jkogut.pl>
2025-11-03 22:28:02 +0100

    zadanie 5

--- lab2/wlasnyGraf/narysujKontrprzyklad.py
+++ lab2/wlasnyGraf/narysujKontrprzyklad.py
@@ -2,23 +2,18 @@ import matplotlib.pyplot as plt
 import networkx as nx
 
 # --- dane ---
-N = 10
+N = 4
 s = 1
-t = 10
-Tmax = 15
+t = 4
+Tmax = 6
 
 # lista łuków (i, j, c, t)
 arcs = [
-    (1, 2, 3, 4),  (1, 3, 4, 9),  (1, 4, 7, 10), (1, 5, 8, 12),
-    (2, 3, 2, 3),  (3, 4, 4, 6),  (3, 5, 2, 2),  (3, 10, 6, 11),
-    (4, 5, 1, 1),  (4, 7, 3, 5),  (5, 6, 5, 6),  (5, 7, 3, 3),
-    (5, 10, 5, 8), (6, 1, 5, 8),  (6, 7, 2, 2),  (6, 10, 7, 11),
-    (7, 3, 4, 6),  (7, 8, 3, 5),  (7, 9, 1, 1),  (8, 9, 1, 2),
-    (9, 10, 2, 2),
+    (1, 2, 1, 5), (1, 3, 5, 1), (2, 4, 1, 5), (3, 4, 5, 1)
 ]
 
 # przykładowa ścieżka spełniająca Tmax=15: 1-2-3-5-7-9-10
-feasible_path = [(1,2), (2,3), (3,5), (5,7), (7,9), (9,10)]
+feasible_path = [(1, 2), (2, 4)]
 
 # --- budowa grafu skierowanego ---
 G = nx.DiGraph()
@@ -30,7 +25,7 @@ for i, j, c, ttime in arcs:
 pos = nx.spring_layout(G, seed=7)  # stały seed = powtarzalny rysunek
 
 # --- rysowanie węzłów i krawędzi ---
-plt.figure(figsize=(20, 14))
+plt.figure(figsize=(10, 7))
 nx.draw_networkx_nodes(G, pos, node_size=900, node_color="#f2f2f2", edgecolors="black")
 nx.draw_networkx_labels(G, pos, labels={n: str(n) for n in G.nodes()}, font_size=12)
 
@@ -54,8 +49,7 @@ nx.draw_networkx_edge_labels(G, pos, edge_labels=labels_c, font_color="red", fon
 nx.draw_networkx_edge_labels(G, pos, edge_labels=labels_t, font_color="blue", font_size=9, label_pos=0.65, rotate=False)
 
 # --- podpis i pokaz ---
-plt.title(f"RCSP: s={s}, t={t}, Tmax={Tmax} | Ścieżka zaznaczona na zielono (czas=15, koszt=13)")
 plt.axis("off")
 plt.tight_layout()
-plt.savefig("wlasny.png", dpi=600)
+plt.savefig("wlasnyKontprzyklad.png", dpi=600)
 
