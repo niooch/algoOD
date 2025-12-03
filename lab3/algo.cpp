a02633a42e0ad46cf0a47b8f9cbeba5901a9129c
Jakub Kogut <jakub@jkogut.pl>
2025-12-03 17:22:27 +0100

    dial

--- lab3/algo.cpp
+++ lab3/algo.cpp
@@ -1,9 +1,8 @@
 //implementacja algorytmow
 #include "algo.h"
-#include <iostream>
 #include <queue>    //do generycznego Dijkstry, zlozonosc push: O(log n)
                     //https://en.cppreference.com/w/cpp/container/priority_queue/push.html
-
+//implementacja generycznego Dijkstry z uzyciem kolejki priorytetowej (min-heap)
 void dijkstra(const Graph& g, int s, std::vector<Distance>& dist){
     int n = static_cast<int>(g.size()-1);
     dist.assign(n+1, INF);
@@ -27,3 +26,48 @@ void dijkstra(const Graph& g, int s, std::vector<Distance>& dist){
     }
 }
 
+void dial(const Graph& g, int s, std::vector<Distance>& dist, int C){
+    int n = static_cast<int>(g.size()-1);
+    dist.assign(n+1, INF);
+    dist[s] = 0;
+
+    //kubelki
+    int binCount = std::max(1, C+1);
+    std::vector<std::vector<int>> buckets(binCount);
+
+    int currentBucket = 0;
+    buckets[0].push_back(s);
+
+    while(true){
+        //znajdz najblizszy niepusty kubelek
+        int steps = 0;
+        while(buckets[currentBucket].empty() && steps < binCount){
+            currentBucket = (currentBucket + 1) % binCount;
+            steps++;
+        }
+
+        //warunek zakonczenia
+        if(steps == binCount && buckets[currentBucket].empty()){
+            break; //wszystkie kubelki puste
+        }
+
+        int u = buckets[currentBucket].back();
+        buckets[currentBucket].pop_back();
+
+        Distance du = dist[u];
+
+        //relaksacja krawedzi
+        for(const auto& [v, w] : g[u]){
+            Distance dv = dist[v];
+            if(du + w < dv){
+                dist[v] = du + w;
+                int idx = static_cast<int>((du + w) % binCount);
+                buckets[idx].push_back(v);
+            }
+        }
+    }
+}
+
+void radixheap(const Graph& g, int s, std::vector<Distance>& dist){
+    
+}
